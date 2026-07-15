17b2bcf9fd30c77308c9c7649f249e0764717e44
Tetlin06 <etlintomas6@gmail.com>
2026-07-15 13:23:41 -0400

    Update alternating_completion_noisy.m

--- src/solver/alternating_completion_noisy.m
+++ src/solver/alternating_completion_noisy.m
@@ -19,62 +19,69 @@
 % matrix. 
 % --------------------------------------------------------------------------
 % We assume the partial inter-distance information comes from a uniform
-% random sample with additive Gaussian noise. 
+% random sample. The information is assumed to be exact.
 % Dist is the full distance matrix. Weight is a binary matrix informing 
 % whether a given entry of Dist is chosen or not.
 % --------------------------------------------------------------------------
 % Rongjie Lai, Abiy Tasissa
 % --------------------------------------------------------------------------
-function [Global_Coordinate, IPM_Recon, output]=alternating_completion_noisy...
-(Dist,Weight, pointInitial, opts,lsopts)
-% aug. lagrangian penalty, noise parameter, and estimate of the rank
-lamda = opts.lamda;
+function [Global_Coordinate, IPM_Recon, output]=alternating_completion_noisy(Dist,Weight,Initial,opts,lsopts)
+% aug. lagrangian penalty and estimate of the rank
+r = opts.r;
 Rk = opts.rank;
 % dim = number of points
-% Dist_squ = D^{2}(i,j) = {d_{i,j}^{2}} 
-[dim,~] = size(Dist);
-% calculate the ground truth of inner-product matrix 
+% Dist = D^{2}(i,j) = {d_{i,j}^{2}} matrix
+[dim , ~] = size(Dist);
+% calculate the ground truth of inner-product matrix
 IPM_Truth = Dist - mean(Dist,2)*ones(1,dim);
 IPM_Truth =  - 1/2*(IPM_Truth - ones(dim,1)*mean(IPM_Truth,1));
 % indices of the randomly chosen entries of D
-% note that, edgeind contains both symmetric indices
-% i.e (1,2) and (2,1)
 [I,J] = find(Weight==1);
 diagind = (1:dim + 1:dim*dim);
 edgeind = I + (J - 1)*dim;
 % the minimization problem has linear constraint R_{Omega}(X) = R_{Omega)(M)
-% now perturbed by noise
-M_noisy = Dist(edgeind);
-b = M_noisy;
+% Represent this constraint as A(X) = b where b = R_{omega}(M)
+M = Dist(edgeind);
+b = M;
 w = 1./(sqrt(b)+1e-8);
-% w = min(w,100);   % optional safeguard
+%w = min(w,100);   % optional safeguard
 % -------------------------------------------------------------------------
 % main algorithm
 % -------------------------------------------------------------------------
-% initialize P, lagrangian multipliers
-P = pointInitial;
+% initialize P, lagrangian multiplier
+
+P = Initial;
+D1 = zeros(length(b),1);
 % initialize energies
+E1 = zeros(opts.maxit,1);
 E = zeros(opts.maxit,1);
-% initialize iteration counters and relative errors
 num_it = 0;
 cre = 1 ;
 % -------------------------------------------------------------------------
-% main iteration: BB method to solve for P 
+% main iteration: BB method to solve for P
 % -------------------------------------------------------------------------
 for i = 1:opts.maxit
     num_it = num_it + 1;
-    % do line search based gradient descent for P
-    P = BBGradient(P,@(P)gradient2(P),lsopts);
+    % do line search based gradient descent for P    
+    P = BBGradient(P,@(P)gradient(P),lsopts);
+    % update multiplier D
+    tmperr = w .* (A_operator(P)-b);
+    D1 = D1 + tmperr;
     % total energy
-    E(i) = 0.5*lamda*norm(w.*(A_operator(P)-b),'fro')^2;
+    E(i) =  sum(sum(P.*P,2)) + 0.5*r*norm(D1,'fro')^2;
+    % calculate the energy E1 
+    E1(i) = 0.5 * norm(tmperr,'fro')^2;
     if opts.printenergy==1
-        fprintf('Iteration %d, TotalE = %f\n',i, E(i));
+        fprintf('Iteration %d, TotalE1 = %f\n',i,E1(i));
     end
     % stopping condition
     if i > 1
         cre = abs(E(i) - E(i-1))/E(i);
     end
-    if(cre < 1e-5)
+    %if(E1(i) < opts.tol && cre < opts.tol)
+    %    break;
+    %end
+    if(cre < opts.tol)
         break;
     end
 end
@@ -100,6 +107,7 @@ Global_Coordinate = real(V(:,1:3)*diag(sqrt(D(1:3))));
 % output parameters: constraint energy E1, total energy E, relative error 
 % in the gram matrix
 % -------------------------------------------------------------------------
+output.E1 = E1(1:num_it);
 output.E =  E(num_it);
 output.ReconError = IPM_err;
 output.numit = num_it;
@@ -108,41 +116,42 @@ output.numit = num_it;
 % R_{\omega}(X) = R_{\omega}(M)
 % -------------------------------------------------------------------------
 function [Y] = A_operator(X)
-% diagonals and off diagonals of X*X'
-X_diag = sum(X.*X,2);
-X_offdiag = sum(X(I,:).*X(J,:),2);
-Y = X_diag(I)+X_diag(J)-2*X_offdiag;
-end
+        % diagonals and off diagonals of X*X'
+        X_diag = sum(X.*X,2);
+        X_offdiag = sum(X(I,:).*X(J,:),2);
+        % resttiction operator
+        Y = X_diag(I)+X_diag(J)-2*X_offdiag;        
+ end
 % -------------------------------------------------------------------------
-% Constructs the adjoint operator A* (see associated paper for details). 
+% Constructs the adjoint operator for A (see associated paper for details). 
 % -------------------------------------------------------------------------
-function [X] = At_operator(y)
-% first part of A^{*}(y) : \sum_{i} y^{1}_{alpha_i} w_{alpha_i}
-X = zeros(dim,dim);
-X(edgeind) = -2*y(1:end);
-X(diagind) =  -sum(X,2);
-end
+    function [X] = At_operator(y)
+        % first part of A^{*}(y) : \sum_{alpha in omega} y^{1}_{alpha} w_{alpha}
+        X = zeros(dim,dim);
+        X(edgeind) = -2*y(1:end);
+        X(diagind) =  -sum(X,2);
+    end
 % -------------------------------------------------------------------------
 % a function handle for the line search BB algorithm
 % -------------------------------------------------------------------------
-function [F,G] = gradient(P)
-tmp1 = A_operator(P)-b;
-tmp1 = w .* tmp1;
-% F is the objective function 
-F = sum(sum(P.*P,2))+ 0.5*lamda*norm(tmp1,'fro')^2;
-% G is the gradient
-G = 2*P+2.0*lamda*At_operator((w.^2).*tmp1)*P;
+    function [F,G] = gradient(P)
+        tmp0 = A_operator(P)-b+D1;
+        tmp = w .* tmp0;
+        % F is the objective function
+        F = sum(sum(P.*P,2))+0.5*r*norm(tmp,'fro')^2;
+        % G is the gradient
+        G = 2*P+2.0*r*At_operator((w.^2).*tmp0)*P;
+    end
+
+    function [F,G] = gradient2(P)
+        tmp0 = A_operator(P)-b+D1;
+        tmp = w .* tmp0;
+        % F is the objective function
+        F = 0.5*r*norm(tmp,'fro')^2;
+        % G is the gradient
+        G = 2.0*r*At_operator((w.^2).*tmp0)*P;
+    end
 end
 
-function [F,G] = gradient2(P)
-tmp1 = A_operator(P)-b;
-tmp1 = w .* tmp1;
-% F is the objective function 
-F = 0.5*lamda*norm(tmp1,'fro')^2;
-% G is the gradient
-G = 2.0*lamda*At_operator((w.^2).*tmp1)*P;
-
-end
-end
 
 
